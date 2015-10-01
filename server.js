7e4a7c34aa543b2804cd3f782341e9a224cb509c
Duong Tran <tq.duong@icloud.com>
2015-10-01 19:03:56 +0700

    Refactor code

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 /// <reference path="typings/tsd.d.ts"/>
 // OpenShift Simple Nodejs application
+// Define constants
+var PORT = 'port';
+var IPADDRESS = 'ipaddress';
+var LOCALHOST = '127.0.0.1';
+var PORTNUMBER = 3000;
+var DEVELOPMENT = 'development';
+var ENV = 'env';
+var ERROR = 'error';
+var NOT_FOUND = 'Not Found';
+var STATUS = 'status';
+var STATUS_404 = 404;
+var STATUS_500 = 500;
+var PUBLIC = 'public';
+var VIEWS = 'views';
 // Define dependencies
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+var serveFavicon = require('serve-favicon');
 var logger = require('morgan');
 // TODO cookieParser
 // TODO bodyParser
@@ -13,31 +27,31 @@ var search = require('./routes/search');
 // Create app
 var app = express();
 // Configure template engine
-app.set('views', path.join(__dirname, 'views'));
+app.set(VIEWS, path.join(__dirname, VIEWS));
 app.set('view engine', 'jade');
 // Configure middleware
-app.use(favicon(path.join(__dirname, 'public', 'img', 'favicon.ico')));
+app.use(serveFavicon(path.join(__dirname, PUBLIC, 'img', 'favicon.ico')));
 app.use(logger('dev'));
 // TODO bodyParser
 // TODO cookieParser
 // Set up static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, PUBLIC)));
 // Define routes
 app.use('/', index);
 app.use('/search', search);
 // Catch 404 and forwarding to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err['status'] = 404;
+    var err = new Error(NOT_FOUND);
+    err[STATUS] = STATUS_404;
     next(err);
 });
 // Error handlers
 // Development error handler
 // will print stacktarce
-if (app.get('env') === 'development') {
+if (app.get(ENV) === DEVELOPMENT) {
     app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
+        res.status(err.status || STATUS_500);
+        res.render(ERROR, {
             message: err.message,
             error: err
         });
@@ -46,16 +60,16 @@ if (app.get('env') === 'development') {
 // Production error handler
 // no stacktrace leaked to user
 app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
+    res.status(err.status || STATUS_500);
+    res.render(ERROR, {
         message: err.message,
         error: {}
     });
 });
 // Start app
-app.set('ipaddress', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
-var server = app.listen(app.get('port'), app.get('ipaddress'), function () {
+app.set(IPADDRESS, process.env.OPENSHIFT_NODEJS_IP || LOCALHOST);
+app.set(PORT, process.env.OPENSHIFT_NODEJS_PORT || PORTNUMBER);
+var server = app.listen(app.get(PORT), app.get(IPADDRESS), function () {
     console.log('Express server listening on port ' + server.address().port);
 });
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -2,31 +2,49 @@
 
 // OpenShift Simple Nodejs application
 
+// Define constants
+const PORT: string        = 'port';
+const IPADDRESS: string   = 'ipaddress';
+const LOCALHOST: string   = '127.0.0.1';
+const PORTNUMBER: number  = 3000;
+
+const DEVELOPMENT: string = 'development';
+const ENV: string         = 'env';
+const ERROR: string       = 'error';
+const NOT_FOUND: string   = 'Not Found';
+
+const STATUS: string      = 'status';
+const STATUS_404: number  = 404;
+const STATUS_500: number  = 500;
+
+const PUBLIC: string      = 'public';
+const VIEWS: string       = 'views';
+
 // Define dependencies
-import express  = require('express');
-import path     = require('path');
-import favicon  = require('serve-favicon');
-import logger   = require('morgan');
+import express      = require('express');
+import path         = require('path');
+import serveFavicon = require('serve-favicon');
+import logger       = require('morgan');
 // TODO cookieParser
 // TODO bodyParser
 // routes dependencies
-var index       = require('./routes/index');
-var search      = require('./routes/search');
+var index           = require('./routes/index');
+var search          = require('./routes/search');
 
 // Create app
-const app = express();
+var app = express();
 
 // Configure template engine
-app.set('views', path.join(__dirname, 'views'));
+app.set(VIEWS, path.join(__dirname, VIEWS));
 app.set('view engine', 'jade');
 
 // Configure middleware
-app.use(favicon(path.join(__dirname, 'public', 'img', 'favicon.ico')));
+app.use(serveFavicon(path.join(__dirname, PUBLIC, 'img', 'favicon.ico')));
 app.use(logger('dev'));
 // TODO bodyParser
 // TODO cookieParser
 // Set up static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, PUBLIC)));
 
 // Define routes
 app.use('/', index);
@@ -34,8 +52,8 @@ app.use('/search', search);
 
 // Catch 404 and forwarding to error handler
 app.use((req, res, next) => {
-    var err = new Error('Not Found');
-    err['status'] = 404;
+    var err = new Error(NOT_FOUND);
+    err[STATUS] = STATUS_404;
     next(err);
 });
 
@@ -43,10 +61,10 @@ app.use((req, res, next) => {
 
 // Development error handler
 // will print stacktarce
-if (app.get('env') === 'development') {
+if (app.get(ENV) === DEVELOPMENT) {
     app.use((err: any, req, res, next) => {
-        res.status(err.status || 500);
-        res.render('error', {
+        res.status(err.status || STATUS_500);
+        res.render(ERROR, {
             message: err.message,
             error: err
         });
@@ -56,17 +74,17 @@ if (app.get('env') === 'development') {
 // Production error handler
 // no stacktrace leaked to user
 app.use((err: any, req, res, next) => {
-    res.status(err.status || 500);
-    res.render('error', {
+    res.status(err.status || STATUS_500);
+    res.render(ERROR, {
         message: err.message,
         error: {}
     });
 });
 
 // Start app
-app.set('ipaddress', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
+app.set(IPADDRESS, process.env.OPENSHIFT_NODEJS_IP || LOCALHOST);
+app.set(PORT, process.env.OPENSHIFT_NODEJS_PORT || PORTNUMBER);
 
-var server = app.listen(app.get('port'), app.get('ipaddress'), function() {
+var server = app.listen(app.get(PORT), app.get(IPADDRESS), function() {
     console.log('Express server listening on port ' + server.address().port);
 });
