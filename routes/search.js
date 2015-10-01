d855ba22b928f582d499aa34ba4932293183fd27
Duong Tran <tq.duong@icloud.com>
2015-10-01 18:41:27 +0700

    Fix River exception when query string is empty

--- routes/search.js
+++ routes/search.js
@@ -5,14 +5,19 @@ var request = require('request');
 var router = express.Router();
 // GET data from elastic and send response
 router.get('/', function (req, res) {
-    request.get('http://localhost:9200/recipesindex/_search?q=_all:' + req.query.q, function (err, response, body) {
-        if (err) {
-            res.send(err);
-        }
-        else if (response.statusCode == 200) {
-            res.send(body);
-        }
-    });
+    if (!req.query.q) {
+        res.send({ 'hits': { 'hits': [] } });
+    }
+    else {
+        request.get('http://localhost:9200/recipesindex/_search?q=_all:' + req.query.q, function (err, response, body) {
+            if (err) {
+                res.send(err);
+            }
+            else if (response.statusCode == 200) {
+                res.send(body);
+            }
+        });
+    }
 });
 module.exports = router;
 //# sourceMappingURL=search.js.map
--- routes/search.ts
+++ routes/search.ts
@@ -8,13 +8,17 @@ var router     = express.Router();
 
 // GET data from elastic and send response
 router.get('/', function(req: express.Request, res: express.Response) {
-    request.get('http://localhost:9200/recipesindex/_search?q=_all:' + req.query.q, function (err: any, response: http.IncomingMessage, body: any) {
-        if (err) {
-            res.send(err);
-        } else if (response.statusCode == 200) {
-            res.send(body);
-        }
-    });
+    if (!req.query.q) {
+        res.send({'hits': {'hits': []}});
+    } else {
+        request.get('http://localhost:9200/recipesindex/_search?q=_all:' + req.query.q, function (err: any, response: http.IncomingMessage, body: any) {
+            if (err) {
+                res.send(err);
+            } else if (response.statusCode == 200) {
+                res.send(body);
+            }
+        });
+    }
 });
 
 module.exports = router;
